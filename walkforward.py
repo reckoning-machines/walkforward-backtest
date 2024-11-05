996960b8baf6c1dc7e51ada9c5d0b91b262a8497
Jed Gore <58391515+reckoning-machines@users.noreply.github.com>
2024-11-05 08:42:55 -0500

    Update walkforward.py
    add docstrings to classes

--- walkforward.py
+++ walkforward.py
@@ -6,7 +6,21 @@ from sklearn.metrics import accuracy_score
 import numpy as np
 
 class WalkForward:
+    """
+    A class representing a recordset walkforward back test.
+
+    Attributes:
+        test_size (float): size in % test in train / test split.
+        target_column (int): single classifier target column in featureset.
+        featureset_columns (list): list of featureset columns to be converted to float.
+        classifier (scikit classifier): classifier object.
+        minimum_dataset_length (float): length in % of initial recordset from which to begin walk forward
+    """
+
     def __init__(self, classifier = None, dataset = None, target_column='',featureset_columns = []):
+        """
+        Initializes WalkForward object
+        """
         self.dataset = dataset
         self.test_size = .2
         self.target_column = target_column
@@ -17,6 +31,13 @@ class WalkForward:
         return
 
     def fit(self):
+        """
+        Fit the classifier
+        Initially set the minimum dataset length, then step over each record
+        instantiating a WalkForwardSingleton for each record
+        then, appending a rolling_df_list with the output of WalkForwardSingleton (a greedy approach for debug)
+        concatenate the rolling df list into a result dataset property
+        """
         len_df = int(len(self.dataset))
         len_end_x = int(round(len_df * self.minimum_dataset_length,0))
         incremental_end_x = len_end_x
@@ -32,7 +53,26 @@ class WalkForward:
         return
 
 class WalkForwardSingleton(multiprocessing.Process):
-    def __init__(self, classifier = None, dataset = None, target_column = '', featureset_columns=[],test_size=.2):
+    """
+    A class representing a singleton of a recordset walkforward back test.
+    Inherits from multi processing Process for threading
+
+    Attributes:
+        test_size (float): size in % test in train / test split.
+        target_column (int): single classifier target column in featureset.
+        featureset_columns (list): list of featureset columns to be converted to float.
+        classifier (scikit classifier): classifier object.
+        dataset (pandas dataframe): pandas dataframe for splitting and fitting
+    """
+
+    def __init__(self, classifier = None, dataset = None, target_column = '', featureset_columns=[], test_size=.2):
+        """
+        Initializes WalkforwardSingleton object
+        split the dataset, fit the classifier,
+        compute predictions, accuracy score and probabilities (assumes the classifer has this method)
+        stores weights in a property
+        converts 'dataset' to 'singleton_dataset' which appends the predictions
+        """
         self.dataset = dataset
         X_train, X_test, y_train, y_test = train_test_split(dataset[featureset_columns], dataset[target_column], test_size = test_size, random_state = 10)
         classifier.fit(X_train, y_train)
@@ -48,4 +88,3 @@ class WalkForwardSingleton(multiprocessing.Process):
         self.singleton_dataset['y_pred'] = self.y_pred
         self.singleton_dataset['y_pred_proba'] = self.y_pred_proba
         return
-
