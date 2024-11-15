e6ea47c7744405fb38ec146066762b43a3bd5fdd
Jed Gore <58391515+reckoning-machines@users.noreply.github.com>
2024-11-15 06:04:38 -0500

    Update walkforward.py

--- walkforward.py
+++ walkforward.py
@@ -1,50 +1,10 @@
 import multiprocessing
 import pandas as pd
 from copy import deepcopy
-from sklearn.model_selection import train_test_split
-import numpy as np
-
-import multiprocessing
-import pandas as pd
-from copy import deepcopy
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 pd.set_option('mode.chained_assignment', None)
 
 import numpy as np
-
-def fit_model(self):
-    """
-    User defined function for fitting a model.
-
-    Inputs:
-    test_size (float): size in % test in train / test split.
-    target_column (int): single classifier target column in featureset.
-    featureset_columns (list): list of featureset columns to be converted to float.
-    classifier (scikit classifier): classifier object.
-    dataset (pandas dataframe): pandas dataframe for splitting and fitting
-    """
-    dataset = self.dataset
-    featureset_columns = self.featureset_columns
-    target_column = self.target_column
-    test_size = self.test_size
-    classifier = self.classifier
-
-    last_date = self.dataset.iloc[-1]['date']
-    X_train, X_test, y_train, y_test = train_test_split(dataset[featureset_columns], dataset[target_column], test_size = test_size, random_state = 10)
-    classifier.fit(X_train, y_train)
-    y_pred = list(classifier.predict(X_test))
-    y_pred_proba = list(classifier.predict_proba(X_test)[:,1])
-    weights = classifier.coef_
-    weights = pd.DataFrame(zip(X_train.columns, np.transpose(weights[0])), columns=['features', 'coef']) 
-    weights['date'] = last_date
-    singleton_dataset = dataset.tail(1)
-    y_pred = y_pred[-1]
-    y_pred_proba = y_pred_proba[-1]
-    singleton_dataset['y_pred'] = y_pred
-    singleton_dataset['y_pred_proba'] = y_pred_proba
-    return singleton_dataset, weights
-
 
 class WalkForward:
     """
@@ -87,13 +47,9 @@ class WalkForward:
         for i in range(len_df - len_end_x):
             incremental_window_data = self.dataset.iloc[0:incremental_end_x]
 
-            clsWalkForwardSingleton = Singleton()
-            clsWalkForwardSingleton.classifier = self.classifier
-            clsWalkForwardSingleton.target_column = self.target_column
-            clsWalkForwardSingleton.featureset_columns = self.featureset_columns
-            clsWalkForwardSingleton.test_size = .2
-            clsWalkForwardSingleton.dataset = incremental_window_data
-            clsWalkForwardSingleton.fit_model = fit_model.__get__(clsWalkForwardSingleton, Singleton)
+            clsWalkForwardSingleton = WalkForwardSingleton(classifier = self.classifier, dataset = incremental_window_data, target_column=self.target_column, featureset_columns=self.featureset_columns)
+            
+            #user defined function
             singleton_dataset, weights = clsWalkForwardSingleton.fit_model()
             
             incremental_end_x = incremental_end_x + 1
@@ -105,6 +61,69 @@ class WalkForward:
         self.weights_dataset = pd.concat(rolling_weights_list)
 
         return
+
+class WalkForwardSingleton(multiprocessing.Process):
+    """
+    A class representing a singleton of a recordset walkforward back test.
+    Inherits from multi processing Process for threading
+    Attributes:
+        test_size (float): size in % test in train / test split.
+        target_column (int): single classifier target column in featureset.
+        featureset_columns (list): list of featureset columns to be converted to float.
+        classifier (scikit classifier): classifier object.
+        dataset (pandas dataframe): pandas dataframe for splitting and fitting
+    """
+
+    def __init__(self, classifier = None, dataset = None, target_column = '', featureset_columns=[], test_size=.2):
+        """
+        Initializes WalkforwardSingleton object
+        split the dataset, fit the classifier,
+        compute predictions, accuracy score and probabilities (assumes the classifer has this method)
+        stores weights in a property
+        converts 'dataset' to 'singleton_dataset' which appends the predictions
+        """
+        self.dataset = dataset
+        self.featureset_columns = featureset_columns
+        self.target_column = target_column
+        self.test_size = test_size
+        self.classifier = classifier
+        return
+    
+    def fit_model(self):        
+        """
+        User defined function for fitting a model.
+
+        Inputs:
+        test_size (float): size in % test in train / test split.
+        target_column (int): single classifier target column in featureset.
+        featureset_columns (list): list of featureset columns to be converted to float.
+        classifier (scikit classifier): classifier object.
+        dataset (pandas dataframe): pandas dataframe for splitting and fitting
+        """
+        from sklearn.model_selection import train_test_split
+        from sklearn.metrics import accuracy_score
+
+        dataset = self.dataset
+        featureset_columns = self.featureset_columns
+        target_column = self.target_column
+        test_size = self.test_size
+        classifier = self.classifier
+
+        last_date = self.dataset.iloc[-1]['date']
+        X_train, X_test, y_train, y_test = train_test_split(dataset[featureset_columns], dataset[target_column], test_size = test_size, random_state = 10)
+        classifier.fit(X_train, y_train)
+        y_pred = list(classifier.predict(X_test))
+        accuracy_score = accuracy_score(y_test, y_pred)
+        y_pred_proba = list(classifier.predict_proba(X_test)[:,1])
+        weights = classifier.coef_
+        weights = pd.DataFrame(zip(X_train.columns, np.transpose(weights[0])), columns=['features', 'coef']) 
+        weights['date'] = last_date
+        singleton_dataset = dataset.tail(1)
+        y_pred = y_pred[-1]
+        y_pred_proba = y_pred_proba[-1]
+        singleton_dataset['y_pred'] = y_pred
+        singleton_dataset['y_pred_proba'] = y_pred_proba
+        return singleton_dataset, weights
 
 class Singleton(multiprocessing.Process):
     """
